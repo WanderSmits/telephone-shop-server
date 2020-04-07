198916f23b6afc6e20201a4e5a9b53096fdc775d
WanderSmits <wandersmits@gmail.com>
2020-04-07 16:34:19 +0200

    made order models, migration, seeds

---
+++ seeders/20200407134414-some-orders.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "orders",
+      [
+        {
+          userId: 1,
+          productId: 2,
+
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 2,
+          productId: 2,
+
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("orders", null, {});
+  },
+};
