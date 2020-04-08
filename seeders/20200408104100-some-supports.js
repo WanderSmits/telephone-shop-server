d5cb9a08449ea7d5a0a67d4fe18a64264e714da2
zoranaism <lazareviczorana@gmail.com>
2020-04-08 13:00:54 +0200

    Support table created

--- seeders/20200408104100-some-supports.js
+++ seeders/20200408104100-some-supports.js
@@ -1,26 +1,37 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert(
+      "supports",
+      [
+        {
+          reason: "Delivery Issue", 
+          subject: "poor service", 
+          description: "I am writing today to complain of the poor service I received from your company on June 12, 2016. I was visited by a representative of That Awful Company, Mr. Madman, at my home on that day.", 
+          link: "https://www.writeexpress.com/complaint.htm",
+          resolved: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          userId: 1
+        },
+        {
+          reason: "Neq Request", 
+          subject: "I would like to buy more", 
+          description: "I am writing today to complain of the poor service I received from your company on June 12, 2016. I was visited by a representative of That Awful Company, Mr. Madman, at my home on that day.", 
+          link: "https://www.writeexpress.com/complaint.htm",
+          resolved: true,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          userId: 2
+        },
+        
+      ],
+      {}
+    );
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+  down: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkDelete("supports", null, {});
+  },
 };
